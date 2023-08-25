670d89e3bb950f7941a1c446fb8ad268d4839fd9
sourajeet_dey <write2sjdey@gmail.com>
2023-08-25 18:52:52 +0530

    Addded basic functionalities

--- src/example/GuessingGame.java
+++ src/example/GuessingGame.java
@@ -7,9 +7,41 @@ import javax.swing.SwingConstants;
 import java.awt.Color;
 import javax.swing.JTextField;
 import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class GuessingGame extends JFrame {
 	private JTextField txtGuess;
+	private JLabel lblOutput;
+	private int theNumber;
+	public void checkGuess() {
+		String guessText = txtGuess.getText();
+		String message = "";
+		int guess = Integer.parseInt(guessText);
+		if(guess < theNumber) {
+			if(theNumber - guess > 25)
+				message = guess + " is too low. Try again.";
+			else
+				message = guess + " is slightly low. Try again.";
+		}
+		else if(guess > theNumber) {
+			if(guess - theNumber > 25)
+				message = guess + " is too high. Try again.";
+			else
+				message = guess + " is slightly high. Try again.";
+		}
+		else {
+			message = guess + " is correct. You win!";
+//			if(guessCount <= 10)
+//				System.out.println("It took you only " + guessCount + " tries. Good job!");
+//			else
+//				System.out.println("It took you " + guessCount + " tries. Can you do better?");
+		}
+		lblOutput.setText(message);
+	}
+	public void newGame() {
+		theNumber =(int) (Math.random()*1000 + 1);
+	}
 	public GuessingGame() {
 		setForeground(new Color(128, 64, 0));
 		setFont(new Font("SansSerif", Font.BOLD | Font.ITALIC, 14));
@@ -39,11 +71,16 @@ public class GuessingGame extends JFrame {
 		txtGuess.setColumns(10);
 		
 		JButton btnGuess = new JButton("Guess!");
+		btnGuess.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				checkGuess();
+			}
+		});
 		btnGuess.setBounds(205, 187, 75, 25);
 		btnGuess.setFont(new Font("Tahoma", Font.BOLD, 13));
 		getContentPane().add(btnGuess);
 		
-		JLabel lblOutput = new JLabel("Enter a number and click Guess!");
+		lblOutput = new JLabel("Enter a number and click Guess!");
 		lblOutput.setHorizontalAlignment(SwingConstants.CENTER);
 		lblOutput.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		lblOutput.setBounds(0, 263, 486, 47);
