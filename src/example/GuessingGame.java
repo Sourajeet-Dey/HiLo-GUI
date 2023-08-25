695d9393599ea845ea4485e7e37f971fddc1aa8a
sourajeet_dey <write2sjdey@gmail.com>
2023-08-25 16:12:27 +0530

    Created new project

---
+++ src/example/GuessingGame.java
@@ -0,0 +1,12 @@
+package example;
+
+import javax.swing.JFrame;
+
+public class GuessingGame extends JFrame {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
